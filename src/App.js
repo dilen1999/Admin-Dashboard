d74500e11ab2dac951ec217e8423c1e97cccc817
Sahayanathan George Dilen <139237758+dilen1999@users.noreply.github.com>
2024-02-13 21:38:14 +0530

    1 st commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Login from './components/login_Page';
+
+function App() {
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
